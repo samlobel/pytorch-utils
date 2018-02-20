2f7e43e482a843a1aade455a0a067ef68484fbd8
Sam Lobel <samlobel@sas.upenn.edu>
2018-02-19 21:48:39 -0500

    setup py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pytorch-utils',
+      version='0.1',
+      description="Sam's Pytorch Utils",
+      url='https://github.com/samlobel/pytorch-utils',
+      author='Sam Lobel',
+      author_email='samuel.lobel@gmail.com',
+      license='MIT',
+      packages=['code'],
+      zip_safe=False)
